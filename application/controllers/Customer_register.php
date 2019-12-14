aab560aae741935c3e1270d50908033f48ce9917
Faisal Malik <faisallmalick@gmail.com>
2019-12-13 18:29:02 -0800

    email configuration added

--- application/controllers/Customer_register.php
+++ application/controllers/Customer_register.php
@@ -23,7 +23,7 @@ class Customer_register extends CI_Controller {
 	  	if($this->form_validation->run())
 	  		{
 			   $verification_key = md5(rand());
-			   $encrypted_password = md5($this->input->post('password'));
+			   $encrypted_password = $this->input->post('password');
 			   $data = array(
 			   'first_name'  => $this->input->post('first_name'),
 			   'last_name'  => $this->input->post('last_name'),
@@ -35,6 +35,51 @@ class Customer_register extends CI_Controller {
 			   'active'	=>  '1'
 	   			);
 			   	$this->Customer_register_model->insert($data);
+
+			   	/*Email code goes here*/
+			   	$this->load->library('email');
+
+			   	$config['protocol'] = 'mail';
+			   	$config['smtp_host'] = 'ssl://smtp.gmail.com';
+			   	$config['smtp_port'] = '465';
+			   	$config['smtp_user'] = 'fassi932@gmail.com';
+				$config['smtp_pass'] = 'lowlight932';
+				$config['charset'] = 'utf-8';
+				//$config['mailpath'] = '/usr/sbin/sendmail';
+				
+				
+				
+				
+				$config['mailtype'] = 'text';
+				$config['wordwrap'] = TRUE;
+
+				$this->email->initialize($config);
+
+
+				$this->email->from('fassi932@gmail.com', 'Faisal Malik');
+				$this->email->to($this->input->post('email'));
+				
+
+				$this->email->subject('User Registration Email');
+
+				$message = "You have been successfully registered, Your Email is: "
+							. $this->input->post('email')
+							. "And Your Password is: "
+							. $this->input->post('password');
+
+
+				$this->email->message($message);
+
+				if($this->email->send())
+			     {
+			      echo 'Email sent.';
+			     }
+			     else
+			    {
+			     show_error($this->email->print_debugger());
+			    }
+
+
 				redirect(base_url()."customer_login");
  			}
 			else
